5973735198fe2bbb68a2f9725955fa7a8375c382
LHJJosh <nicholasdare2@gmail.com>
2022-06-21 00:51:22 +0800

    new branch for booking

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\nicho\OneDrive\50.003_ESC_Project\env
+set VIRTUAL_ENV=C:\Users\nicho\Documents\50.003_ESC_Project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
