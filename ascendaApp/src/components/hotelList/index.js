1755a5be159ebee217629461528cc0474f659317
rehohoho <ernie.birdy.ho@gmail.com>
2022-06-22 18:22:53 +0800

    [ui/HotelList] Add component css

--- ascendaApp/src/components/hotelList/index.js
+++ ascendaApp/src/components/hotelList/index.js
@@ -4,35 +4,43 @@ import Divider from '@mui/material/Divider';
 
 import { HotelListItem } from './hotelListItem.js'
 import { HotelListCard } from './hotelListCard.js'
+import { HotelDropdown } from './hotelDropdown.js'
 
+import "./styles.css";
 
 class HotelListInternal extends React.Component {
   render() {
-    return <List sx={{ width: '60%', bgcolor: 'background.paper', padding: '0px'}}>
-      {/* <HotelListItem name='Cindy Baker'
-                     primaryText='Oui Oui'
-                     secondary='Sandra Adams'
-                     secondaryText=" — Do you have Paris recommendations? Have you ever…"/> */}
-      <HotelListCard hotelName='Studio M Hotel'
-                     hotelImage={require('../../assets/cardmedia_hotel1.jpg')}
-                     hotelAddress='3 Nanson Road'
-                     hotelPrice='450'
-                     hotelDeal='1 for 1 ??!?'/>
-      
-      <Divider variant='inset' component='li' />
-      <HotelListCard hotelName='Park Hotel Clarke Quay'
-                     hotelImage={require('../../assets/cardmedia_hotel2.jpg')}
-                     hotelAddress='1 Unity Street'
-                     hotelPrice='302'
-                     hotelDeal='2 for 1 LOL'/>
-      
-      <Divider variant='inset' component='li' />
-      <HotelListCard hotelName='Aqueen Hotel Paya Lebar'
-                     hotelImage={require('../../assets/cardmedia_hotel3.jpg')}
-                     hotelAddress='33 Jalan Afifi'
-                     hotelPrice='192'
-                     hotelDeal='3 for 1 :O'/>
-    </List>
+    return <div className='HotelList' width='60%'>
+      <div className='selectionOption'>
+        <HotelDropdown/>
+      </div>
+
+      <List sx={{ bgcolor: 'background.paper', padding: '0px'}}>
+        {/* <HotelListItem name='Cindy Baker'
+                      primaryText='Oui Oui'
+                      secondary='Sandra Adams'
+                      secondaryText=" — Do you have Paris recommendations? Have you ever…"/> */}
+        <HotelListCard hotelName='Studio M Hotel'
+                      hotelImage={require('../../assets/cardmedia_hotel1.jpg')}
+                      hotelAddress='3 Nanson Road'
+                      hotelPrice='450'
+                      hotelDeal='1 for 1 ??!?'/>
+        
+        <Divider variant='inset' component='li' />
+        <HotelListCard hotelName='Park Hotel Clarke Quay'
+                      hotelImage={require('../../assets/cardmedia_hotel2.jpg')}
+                      hotelAddress='1 Unity Street'
+                      hotelPrice='302'
+                      hotelDeal='2 for 1 LOL'/>
+        
+        <Divider variant='inset' component='li' />
+        <HotelListCard hotelName='Aqueen Hotel Paya Lebar'
+                      hotelImage={require('../../assets/cardmedia_hotel3.jpg')}
+                      hotelAddress='33 Jalan Afifi'
+                      hotelPrice='192'
+                      hotelDeal='3 for 1 :O'/>
+      </List>
+    </div>
   }
 }
 
