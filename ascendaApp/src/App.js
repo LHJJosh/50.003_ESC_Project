50cadbbc397efabb49b841178cf149ce61b7e0ed
rehohoho <ernie.birdy.ho@gmail.com>
2022-06-24 17:42:47 +0800

    [ui/Index] Move components from index to App

--- ascendaApp/src/App.js
+++ ascendaApp/src/App.js
@@ -1,25 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+import Bookings from "./pages/Bookings";
+import HomePage from "./components/homePage";
+import { HotelList } from './components/hotelList';
+import { HotelSearch } from './components/hotelSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+
+
+class AppInternal extends React.Component {
+  render() {
+    return <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Layout />}> */}
+          {/* <Route index element={<HomePage />} /> */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="*" element={<NoPage />} />
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  }
 }
 
-export default App;
+export const App = AppInternal;
--- ascendaApp/src/index.js
+++ ascendaApp/src/index.js
@@ -1,29 +1,17 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import Bookings from "./pages/Bookings";
-import HomePage from "./components/indexPage/indexPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './App';
+import reportWebVitals from './reportWebVitals';
 
-export default function Index() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="blogs" element={<Blogs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="bookings" element={<Bookings />} />
-        <Route path="app" element={<App />} />
-        <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
