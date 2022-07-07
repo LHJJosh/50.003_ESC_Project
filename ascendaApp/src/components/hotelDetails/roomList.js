5374ca01307c98751d8e44999a1344f0fb57e0fa
James Lamberd <nicholasdare2@gmail.com>
2022-07-07 16:58:54 +0800

    added links to buttons

--- ascendaApp/src/components/hotelDetails/roomList.js
+++ ascendaApp/src/components/hotelDetails/roomList.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './styles.css';
+import { Link } from "react-router-dom";
 
 export function RoomsCard(props) {
 
-    function onClickHandler(){
-      alert('Implement redirect to Booking Page')
-    }
 
     function RoomCard(props){
         return(
@@ -18,7 +16,9 @@ export function RoomsCard(props) {
                         <h3>{props.roomName}</h3>
                         <p>{props.roomRate}</p>
                     </div>
-                    <button className="bookNowButton" onClick={onClickHandler}>Book Now!</button>
+                    <button component={Link} to="/bookings" className="bookNowButton">
+                      Book Now!
+                    </button>
                 </div>
             </div>
         )
