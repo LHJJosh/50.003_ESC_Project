fe309360e1bab71ba5b0b3344641e54e4f0f7bc9
Beverley Chee <bubble.beverley@gmail.com>
2022-07-07 11:09:18 +0800

    destination search link to hotel search

--- ascendaApp/src/components/homePage/index.js
+++ ascendaApp/src/components/homePage/index.js
@@ -3,15 +3,23 @@ import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Card from '@mui/material/Card';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
 import "./styles.css";
 
+
 export default function HomePage() {
+  const [num, setNum] = React.useState('');
+  const handleChange = (event) => {
+    setNum(event.target.value);
+  };
+
   return(
     <Card className='search'>
        <CssBaseline />
@@ -19,15 +27,108 @@ export default function HomePage() {
             sx={{
               marginLeft: 2,
               marginRight: 2,
-              marginTop: 6,
+              marginTop: 2,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
             }}
             >
-              
+              <Typography component="h1" variant="h5" align = "left">
+                DESTINATION
+              </Typography>
+              <Box component="form" sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      id="Destination"
+                      label="Destination"
+                      inputProps={{style: {fontSize: 14, height : 20}}} // font size of input text
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField 
+                        type="date"
+                        required
+                        size='small'
+                        id="checkInDay"
+                        name="Check In Day"
+                    />      
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField 
+                        type="date"
+                        required
+                        size='small'
+                        id="checkOutDay"
+                        name="Check Out Day"
+                    />      
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <InputLabel id="rooms">Rooms</InputLabel>
+                      <Select
+                        labelId="rooms"
+                        id="rooms"
+                        value={num}
+                        label="Rooms"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <InputLabel id="adults">Adults</InputLabel>
+                      <Select
+                        labelId="adults"
+                        id="adults"
+                        value={num}
+                        label="Adults"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <FormControl fullWidth>
+                      <InputLabel id="rooms">Children</InputLabel>
+                      <Select
+                        labelId="children"
+                        id="children"
+                        value={num}
+                        label="Children"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={0}>0</MenuItem>
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                    component={Link} to="/hotelsearch"
+                    fullWidth
+                    variant="contained"
+                     >
+                      Search Destination
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Box>
             </Box>
-    </Card>
+      </Card>
       // <div class="search">
       //     Destination
       //     <br></br><input type="text" placeholder="Search Destination"/>
