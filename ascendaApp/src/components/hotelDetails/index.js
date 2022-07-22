0bd5ff4e8e371004942d6645af17570b9080f753
James Lamberd <nicholasdare2@gmail.com>
2022-07-22 13:01:08 +0800

    updating some stuff and comments

--- ascendaApp/src/components/hotelDetails/index.js
+++ ascendaApp/src/components/hotelDetails/index.js
@@ -35,6 +35,11 @@ export function DetailsPage() {
     }
   });
 
+ 
+  //const myRef = useRef(null)
+
+  //const executeScroll = () => myRef.current.scrollIntoView() 
+
   return (
     <div className="detailsPage">
       <HeaderCard hotelName={state.name} hotelAddress={state.address} hotelImg="https://d2ey9sqrvkqdfs.cloudfront.net/diH7/10.jpg"/>
@@ -42,10 +47,11 @@ export function DetailsPage() {
                    detailsHeader2="Amenities" 
                    detailsText1="With a stay at The Fullerton Hotel Singapore, you'll be centrally located in Singapore, steps from Cavenagh Bridge and Anderson Bridge.  This 5-star hotel is close to Chinatown Heritage Center and Universal Studios Singapore®."
                    detailsText2="Pamper yourself with a visit to the spa, which offers massages, body treatments, and facials. If you're looking for recreational opportunities, you'll find an outdoor pool and a fitness center. This Colonial hotel also features complimentary wireless Internet access, concierge services, and gift shops/newsstands. Guests can get to nearby shops on the complimentary shuttle."/>
-      <RoomsCard roomName1="Test room 1" 
+      <RoomsCard //ref={myRef}
+                 roomName1="Room 1" 
                  roomImg1="https://d2ey9sqrvkqdfs.cloudfront.net/diH7/2.jpg" 
                  roomRate1="$Test1"
-                 roomName2="Test room 2" 
+                 roomName2="Room 2" 
                  roomImg2="https://d2ey9sqrvkqdfs.cloudfront.net/diH7/4.jpg" 
                  roomRate2="$Test2"/>
       <MapsCard hotelName={state.name}
