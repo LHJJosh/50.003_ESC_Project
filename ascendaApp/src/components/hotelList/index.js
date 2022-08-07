06e62eac4d11b9ae34232ff27980e28ff03b72d9
rehohoho <ernie.birdy.ho@gmail.com>
2022-08-07 18:31:30 +0800

    [ui/Search] Remove local storage cache and old UI comments

--- ascendaApp/src/components/hotelList/index.js
+++ ascendaApp/src/components/hotelList/index.js
@@ -25,31 +25,6 @@ class HotelListInternals extends React.Component {
     }
   }
 
-  /*|| JSON.parse(localStorage.getItem('state'))
-  componentDidMount() {
-    const json = window.localStorage.getItem('destination_uid')
-    const destination_uid = JSON.parse(json)
-    this.setState({destination_uid: destination_uid })
-  }
-  componentDidUpdate(prevProps, prevStates){
-    const json = JSON.stringify(this.state.queryParams.destination_uid)
-    window.localStorage.setItem('destination_uid', json)
-  }
-  /*
-  JSON.parse(localStorage.getItem('state')) || 
-  useEffect= (() => {
-    this.setState(JSON.parse(window.localStorage.getItem('state')));
-  }, []);
-
-  useEffect = (() => {
-    window.localStorage.setItem('state', this.state);
-  }, [this.state]);
-
-  setState(state) {
-    localStorage.setItem('state', JSON.stringify(state));
-    super.setState(state);
-  }*/
-
   updateHotelInfo = async (queryUrl) => {
     if (typeof queryUrl !== 'undefined') {
       console.log(queryUrl);
